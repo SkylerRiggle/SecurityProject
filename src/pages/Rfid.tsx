bc864caa9d9f78d166ae829ce22745886d129036
Skyler Riggle <skyler32702@gmail.com>
2023-11-13 17:55:53 -0600

    Authentication Panel

--- src/pages/Rfid.tsx
+++ src/pages/Rfid.tsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 
-const HANDSHAKE = 'A';
-const ACCEPT = 'B';
-
 enum Status
 {
     NOT_CONNECTED = 0,
@@ -14,6 +11,13 @@ enum Status
 const RfidPage = () =>
 {
     const [status, setStatus] = useState<Status>(Status.NOT_CONNECTED);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    const HANDSHAKE = 'A';
+    const ACCEPT = 'B';
+
+    const targetKey = "8A3A4580";
+    const [key, setKey] = useState<string>("N/A")
 
     const attemptConnection = async () =>
     {
@@ -29,7 +33,20 @@ const RfidPage = () =>
                     switch (status)
                     {
                         case Status.CONNECTED:
-                            return <></>;
+                            return <>
+                                <h5>Target Key: <span className="fw-normal">{targetKey}</span></h5>
+                                <h5>Last Key: <span className="fw-normal">{key}</span></h5>
+                                <div className="d-flex align-items-center mt-3">
+                                    <div className="bg-success rounded-circle border me-3"
+                                    style={{ width: "25px", height: "25px" }} />
+                                    <span>CONNECTED</span>
+                                </div>
+                                <div className="d-flex align-items-center mt-3">
+                                    <div className={`${isAuthenticated ? "bg-success" : "bg-danger"} rounded-circle border me-3`}
+                                    style={{ width: "25px", height: "25px" }} />
+                                    <span>AUTHENTICATED</span>
+                                </div>
+                            </>;
                         case Status.CONNECTING:
                             return <div className="d-flex justify-content-center align-items-center">
                                 <div className="spinner-border text-light me-3" />
