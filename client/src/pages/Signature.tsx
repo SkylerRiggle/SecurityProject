3289852f7fc7d44e80a7d5b62ad77a76fe74b5f8
Skyler Riggle <skyler32702@gmail.com>
2023-11-21 13:45:36 -0600

    Compressed Slope Calculation

--- client/src/pages/Signature.tsx
+++ client/src/pages/Signature.tsx
@@ -1,11 +1,53 @@
 import { useRef, useState } from "react";
 import Button from "react-bootstrap/Button";
-import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
+import { Point, ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
+
+const TOLERANCE = 0.05;
 
 const SignaturePage = () =>
 {
     const canvas = useRef<ReactSketchCanvasRef | null>(null);
+
     const [message, setMessage] = useState<string>("N/A");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const reducePoints = (points: Point[]): number[] => 
+    {
+        const slopes: number[] = [];
+
+        let idx = 0;
+        while (idx < points.length - 1)
+        {
+            const p1 = points[idx];
+            const p2 = points[idx + 1];
+
+            const difX = p2.x - p1.x;
+            const difY = p2.y - p1.y;
+
+            if (!(difX || difY))
+            {
+                points.splice(idx + 1, 1);
+                continue;
+            }
+
+            slopes.push(difX ? difY / difX : Infinity);
+            idx++;
+        }
+
+        idx = 0;
+        while (idx < slopes.length - 1)
+        {
+            const diff = Math.abs(slopes[idx] - slopes[idx + 1]);
+            if (diff <= TOLERANCE || !diff)
+            {
+                slopes.splice(idx + 1, 1);
+                continue;
+            }
+            idx++;
+        }
+
+        return slopes;
+    }
 
     return (<div className="d-flex justify-content-center align-items-center flex-column" style={{ minHeight: "100vh" }}>
         <h3 className="mb-4">Sign Here!</h3>
@@ -14,13 +56,26 @@ const SignaturePage = () =>
                 strokeWidth={4}
                 strokeColor="#FF0000"
                 ref={canvas}
+                className={`${loading && "d-none"}`}
             />
         </div>
 
         <h5 className="mt-4">System Message: {message}</h5>
         <div className="mt-4 bg-secondary p-3 rounded border border-dark">
-            <Button className="me-2" onClick={() => {
-                console.log(canvas.current?.exportPaths())
+            <Button className="me-2" onClick={async () => {
+                const cur = canvas.current;
+                if (!cur) { return; }
+                setLoading(true);
+
+                const pointCollection = [];
+                const paths = await cur.exportPaths();
+                for (const path of paths)
+                {
+                    pointCollection.push(reducePoints(path.paths));
+                }
+
+                cur.clearCanvas();
+                setLoading(false);
             }}>
                 Submit
             </Button>
